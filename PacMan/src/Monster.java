6e9e6a4f5d2babe88995840850277029655f4de4
toli2003 <toli2003@student.miun.se>
2021-10-21 23:01:55 +0200

    Monster moving in a rectangle!

--- PacMan/src/Monster.java
+++ PacMan/src/Monster.java
@@ -1,5 +1,7 @@
 
 import java.awt.event.KeyEvent;
+
+import javax.print.attribute.standard.ColorSupported;
 import javax.swing.*;
 import java.awt.*;
 
@@ -9,13 +11,34 @@ public class Monster implements Character {
     final int monsterSize = 30;
     final int moveDistance = 1;
     char lastKey;
-    boolean firsttime = true;
+    String direction = "up";
+    Color color = Color.RED;
 
     public Monster(int x, int y) {
         this.x = x;
         this.y = y;
         System.out.println("Creating Monster at" + x + ", " + y);
 
+    }
+
+    private String getDirection() {
+        System.out.println("GETTING DIRECTION!");
+        if (x == 0 && y > 0) {
+            direction = "up";
+
+        } else if (x == 800 && y == 0) {
+            direction = "down";
+
+        } else if (y == 0 && x > 0) {
+            direction = "right";
+
+        } else if (y == 600 && x == 800) {
+            direction = "left";
+        } else if (x == 0 && y == 0) {
+            direction = "right";
+        }
+
+        return direction;
     }
 
     @Override
@@ -26,18 +49,32 @@ public class Monster implements Character {
 
     @Override
     public void doMove() {
-        int start = (int) Math.random();
 
-        System.out.println(start);
+        switch (getDirection()) {
+        case "left":
+            x = x - moveDistance;
+            break;
+        case "right":
+            x = x + moveDistance;
+            break;
+        case "up":
+            y = y - moveDistance;
+            break;
+        case "down":
+            y = y + moveDistance;
+            // color = Color.GREEN;
+        default:
+            break;
+        }
 
-        System.out.println("Monster is at " + x + ", " + y);
-
+        String debug = String.format("monster is at %d, %d", x, y);
+        System.out.println(debug);
     }
 
     @Override
     public void draw(Graphics g) {
         System.out.println("Drawing monster from drawMonster");
-        g.setColor(Color.RED);
+        g.setColor(color);
         g.fillOval(x, y, monsterSize, monsterSize);
 
     }
