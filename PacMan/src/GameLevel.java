aa7abfae4f5491d7b44240f6513f4396d67aeb9a
toli2003 <toli2003@student.miun.se>
2021-10-21 21:58:48 +0200

    only linting.

--- PacMan/src/GameLevel.java
+++ PacMan/src/GameLevel.java
@@ -11,9 +11,9 @@ import java.nio.file.Paths;
 public class GameLevel extends JComponent {
     int level;
     PacPanel pacpanel;
-    //byte[] levelBytes;
-    //List<String> levelMap;
-    //List<Byte> levelBytes;
+    // byte[] levelBytes;
+    // List<String> levelMap;
+    // List<Byte> levelBytes;
     byte[] readLevel;
     BufferedImage wall, space;
 
@@ -30,18 +30,17 @@ public class GameLevel extends JComponent {
         System.out.println("opening file " + path.toString() + "...");
 
         try {
-            //levelMap = Files.readAllLines(path);
+            // levelMap = Files.readAllLines(path);
             readLevel = Files.readAllBytes(path);
 
         } catch (IOException e) {
             System.out.println("error opening file ");
-            //e.printStackTrace();
+            // e.printStackTrace();
         }
 
         /*
-        System.out.println(readLevel.length);
-        List<?> test =
-                Stream.of(readLevel).collect(Collectors.toList());
+         * System.out.println(readLevel.length); List<?> test =
+         * Stream.of(readLevel).collect(Collectors.toList());
          */
 
     }
@@ -57,7 +56,6 @@ public class GameLevel extends JComponent {
         }
     }
 
-
     protected void paintComponent(Graphics g) {
 
         // Calculate grid size
@@ -66,23 +64,19 @@ public class GameLevel extends JComponent {
 
         // Iterate over map level array
         System.out.println("GameLevel::paintComponent called");
-        g.drawImage(wall,200,200,this);
-        g.fillRect(300,300,100,100);
+        g.drawImage(wall, 200, 200, this);
+        g.fillRect(300, 300, 100, 100);
     }
-
 
     /*
-    protected void drawMap(Graphics g) {
-
-        // Calculate grid size
-        double gridWidth = pacpanel.width / 30;
-        double gridHeight = pacpanel.height / 30;
-
-        // Iterate over map level array
-        System.out.println("GameLevel::paintComponent called");
-    }
-
+     * protected void drawMap(Graphics g) {
+     * 
+     * // Calculate grid size double gridWidth = pacpanel.width / 30; double
+     * gridHeight = pacpanel.height / 30;
+     * 
+     * // Iterate over map level array
+     * System.out.println("GameLevel::paintComponent called"); }
+     * 
      */
-
 
 }
