97c807a30ff6e3777cdc56ab42306e3d440f58cb
toli2003 <toli2003@student.miun.se>
2021-10-23 23:28:28 +0200

    animated pacman

--- PacMan/src/Pacman.java
+++ PacMan/src/Pacman.java
@@ -17,7 +17,9 @@ class Pacman extends JComponent implements Character {
     char direction = 'R';
 
     ArrayList<BufferedImage> pacImages = new ArrayList<BufferedImage>();
-    BufferedImage currentImg;
+    BufferedImage currentImgBig;
+    BufferedImage currentImgSmall;
+    Boolean animation = true;
 
     public Pacman(int x, int y) {
         this.x = x;
@@ -27,13 +29,19 @@ class Pacman extends JComponent implements Character {
         // Load images.
         try {
 
-            pacImages.add(ImageIO.read(this.getClass().getResource("resources/pac/pacLeft.png")));
-            pacImages.add(ImageIO.read(this.getClass().getResource("resources/pac/pacRight.png")));
-            pacImages.add(ImageIO.read(this.getClass().getResource("resources/pac/pacUp.png")));
-            pacImages.add(ImageIO.read(this.getClass().getResource("resources/pac/pacDown.png")));
+            pacImages.add(ImageIO.read(this.getClass().getResource("resources/PacMan/pacLeftBig.png"))); // 0
+            pacImages.add(ImageIO.read(this.getClass().getResource("resources/PacMan/pacRightBig.png"))); // 1
+            pacImages.add(ImageIO.read(this.getClass().getResource("resources/PacMan/pacUpBig.png"))); // 2
+            pacImages.add(ImageIO.read(this.getClass().getResource("resources/PacMan/pacDownBig.png"))); // 3
+
+            pacImages.add(ImageIO.read(this.getClass().getResource("resources/PacMan/pacLeftSmall.png"))); // 4
+            pacImages.add(ImageIO.read(this.getClass().getResource("resources/PacMan/pacRightSmall.png"))); // 5
+            pacImages.add(ImageIO.read(this.getClass().getResource("resources/PacMan/pacUpSmall.png"))); // 6
+            pacImages.add(ImageIO.read(this.getClass().getResource("resources/PacMan/pacDownSmall.png"))); // 7
 
             // Initial pic.
-            currentImg = pacImages.get(0);
+            currentImgBig = pacImages.get(0);
+            currentImgSmall = pacImages.get(4);
 
         } catch (IOException e) {
             System.out.println("Pacmans bild kunde inte hämtas: " + e.getMessage());
@@ -41,7 +49,7 @@ class Pacman extends JComponent implements Character {
     }
 
     public BufferedImage getImage() {
-        return currentImg;
+        return currentImgBig;
     }
 
     /**
@@ -50,23 +58,23 @@ class Pacman extends JComponent implements Character {
     public void keyPressed(KeyEvent e) {
 
         switch (e.getKeyCode()) {
-            case KeyEvent.VK_UP:
-                lastKey = 'U'; // Up
-                break;
-            case KeyEvent.VK_DOWN:
-                lastKey = 'D'; // Down
-                break;
-            case KeyEvent.VK_LEFT:
-                lastKey = 'L'; // Left
-                break;
-            case KeyEvent.VK_RIGHT:
-                lastKey = 'R'; // Right
-                break;
-            case 27:
-                lastKey = 'E'; // Escape (Stop playing)
-                break;
-            case 80:
-                lastKey = 'P'; // P (Pause)
+        case KeyEvent.VK_UP:
+            lastKey = 'U'; // Up
+            break;
+        case KeyEvent.VK_DOWN:
+            lastKey = 'D'; // Down
+            break;
+        case KeyEvent.VK_LEFT:
+            lastKey = 'L'; // Left
+            break;
+        case KeyEvent.VK_RIGHT:
+            lastKey = 'R'; // Right
+            break;
+        case 27:
+            lastKey = 'E'; // Escape (Stop playing)
+            break;
+        case 80:
+            lastKey = 'P'; // P (Pause)
         }
     }
 
@@ -74,51 +82,60 @@ class Pacman extends JComponent implements Character {
 
         // todo: dont hardcode 2*gridsize! (60x60)
         if (lastKey != direction) {
-            // Only change direction to up / down when we are horizontally in center of a grid
+            // Only change direction to up / down when we are horizontally in center of a
+            // grid
             if (lastKey == 'U' || lastKey == 'D') {
-                if ( x % 60 <= 1 ) {
+                if (x % 60 <= 1) {
                     direction = lastKey;
                     System.out.println("changing direction to " + lastKey + " at pos " + x + ", " + y);
-                };
+                }
+                ;
             }
-            // Only change direction to left / right when we are vertically in center of a grid
+            // Only change direction to left / right when we are vertically in center of a
+            // grid
             if (lastKey == 'L' || lastKey == 'R') {
-                if ( y % 60 <= 1 ) {
+                if (y % 60 <= 1) {
                     direction = lastKey;
                     System.out.println("changing direction to " + lastKey + " at pos " + x + ", " + y);
-                };
+                }
+                ;
             }
         }
 
-        //switch (lastKey) {
+        // switch (lastKey) {
         switch (direction) {
         case 'U':
             y = y - moveDistance;
-            currentImg = pacImages.get(2);
+            currentImgBig = pacImages.get(2);
+            currentImgSmall = pacImages.get(6);
             break;
         case 'D':
-            currentImg = pacImages.get(3);
+            currentImgBig = pacImages.get(3);
+            currentImgSmall = pacImages.get(7);
             y = y + moveDistance;
             break;
         case 'L':
-            currentImg = pacImages.get(0);
+            currentImgBig = pacImages.get(0);
+            currentImgSmall = pacImages.get(4);
             x = x - moveDistance;
             break;
         case 'R':
-            currentImg = pacImages.get(1);
+            currentImgBig = pacImages.get(1);
+            currentImgSmall = pacImages.get(5);
             x = x + moveDistance;
             break;
         }
         // DEBUG
-        //System.out.println("pacman moved to " + x + ", " + y);
+        // System.out.println("pacman moved to " + x + ", " + y);
     }
 
     public void draw(Graphics g) {
-        g.drawImage(currentImg, x, y, pacSize, pacSize, null);
+        animation = !animation;
+        g.drawImage(animation ? currentImgBig : currentImgSmall, x, y, pacSize, pacSize, null);
     }
 
     public Rectangle getRectangle() {
-        return new Rectangle(x-2, y-2, pacSize+4, pacSize+4);
+        return new Rectangle(x - 2, y - 2, pacSize + 4, pacSize + 4);
     }
 
 }
