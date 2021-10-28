bd7ebec0a8057cd158b765ed41fc8849c54c1b97
Rasmus Djupedal <radj2000@student.miun.se>
2021-10-28 12:53:14 +0200

    - Door implemented

--- PacMan/src/PacPanel.java
+++ PacMan/src/PacPanel.java
@@ -83,7 +83,8 @@ class PacPanel extends JPanel {
         AbstractFactory pacManFactory = FactoryProducer.getFactory(true);
         AbstractFactory ghostFactory = FactoryProducer.getFactory(false);
 
-        pacman = pacManFactory.getCharacter("pacman", 30, 30);
+        // todo: replace harcoded startpos with Maze.INSTANCE.getPacmanX() & Maze.INSTANCE.getPacmanY()
+        pacman = pacManFactory.getCharacter("pacman", 400, 690);
 
         // Lägger till monster, 300 + i*30 är för att skapa lite space mellen dom, då
         // dom just nu följer samma rörelsemönster.
