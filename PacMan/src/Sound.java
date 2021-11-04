6f9194639256cf57050c6b4a87a39850c90e9e2a
Rasmus Djupedal <radj2000@student.miun.se>
2021-11-04 20:10:20 +0100

    - Added some comments

--- PacMan/src/Sound.java
+++ PacMan/src/Sound.java
@@ -3,6 +3,11 @@ import javax.sound.sampled.Clip;
 import java.io.File;
 import java.util.ArrayList;
 
+/**
+ * Singleton class for playing sound effects.
+ * Original sounds are taken from https://freesound.org
+ */
+
 public class Sound {
     protected static final Sound INSTANCE = new Sound();
     private String[] sounds = {"eat", "die", "kill", "gameover", "mazefinished", "candy"};
@@ -10,8 +15,11 @@ public class Sound {
     private ArrayList<File> soundFiles = new ArrayList<>();
     private ArrayList<Clip> soundClips = new ArrayList<>();
 
+    /**
+     * Singleton, private constructor.
+     * Reads the sound files and stores the audio clips in an array
+     */
     private Sound() {
-
         try {
             int i = 0;
             for (String sound : sounds) {
@@ -25,6 +33,10 @@ public class Sound {
 
     }
 
+    /**
+     * Plays a sound
+     * @param s name of soundeffect
+     */
     protected void play(String s) {
         int soundIndex = 0;
 
@@ -60,6 +72,4 @@ public class Sound {
             soundClips.get(soundIndex).start();
         }
     }
-
-
 }
