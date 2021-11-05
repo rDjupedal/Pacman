6b4459ae70611824def0477f49e2576b0184dec8
toli2003 <toli2003@student.miun.se>
2021-11-05 09:34:10 +0100

    Comments and problems done FrightenedWandering.

--- PacMan/src/FrightenedWandering.java
+++ PacMan/src/FrightenedWandering.java
@@ -1,18 +1,30 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Random;
 
+/**
+ * Runs around in the maze, randomly. No real target, but picks a random
+ * direction at each possible point. Used when PacMan has eaten a power up
+ * Candy.
+ * 
+ * @author Tobias Liljeblad & Rasmus Djupedal
+ */
 public class FrightenedWandering implements IFrightenedBehaviour {
 
-    String previousMove = "";
-
+    /**
+     * Picks a random next step based on the possible moves.
+     * 
+     * @param x                  the x position of the ghost at the time of calling
+     * 
+     * @param y                  the y position of the ghost at the time of calling
+     * @param possibleMovesArray the array of possible moves for the ghost at the
+     *                           coordinate x,y.
+     */
     @Override
     public String FrightenedBehaviour(int x, int y, ArrayList<String> possibleMovesArray) {
 
         int randomInt = new Random().nextInt(possibleMovesArray.size());
 
-        previousMove = possibleMovesArray.get(randomInt);
-        return previousMove;
+        return possibleMovesArray.get(randomInt);
 
     }
 
