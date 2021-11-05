623caabccb847ed833a7cb88aa193d43b5f79b83
toli2003 <toli2003@student.miun.se>
2021-11-05 09:31:34 +0100

    comments and problems done ChaseRandom

--- PacMan/src/ChaseRandom.java
+++ PacMan/src/ChaseRandom.java
@@ -1,26 +1,32 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Random;
 
 /**
  * runs around in the maze, randomly. No real target, but picks a random
  * direction at each possible point.
+ * 
+ * @author Tobias Liljeblad & Rasmus Djupedal
  */
 public class ChaseRandom implements IChaseBehaviour {
 
-    String previousMove = "";
-    String direction;
-
+    /**
+     * Picks a random direction of the possible directions.
+     * 
+     * @param x                  the x position of the ghost at the time of calling
+     * 
+     * @param y                  the y position of the ghost at the time of calling
+     * @param possibleMovesArray the array of possible moves for the ghost at the
+     *                           coordinate x,y.
+     */
     @Override
     public String chase(int x, int y, ArrayList<String> possibleMovesArray) {
 
-        // Craetes a random int between 0 and amount of possible moves.
+        // Creates a random int between 0 and amount of possible moves.
         int randomInt = new Random().nextInt(possibleMovesArray.size());
 
-        // Stores direction to next time.
-        previousMove = possibleMovesArray.get(randomInt);
         // Returns direction to caller.
-        return previousMove;
+
+        return possibleMovesArray.get(randomInt);
 
     }
 
