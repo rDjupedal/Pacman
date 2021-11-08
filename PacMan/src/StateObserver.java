1cd814f1b72d4c2f6996b70c3d82018ee0f54fdf
toli2003 <toli2003@student.miun.se>
2021-11-08 08:45:40 +0100

    added comments

--- PacMan/src/StateObserver.java
+++ PacMan/src/StateObserver.java
@@ -1,3 +1,9 @@
+
+/**
+ * Interface of observers for Observer pattern.
+ * 
+ * @author Tobias Liljeblad & Rasmus Djupedal
+ */
 public interface StateObserver {
     void updateState(String currentState);
 
