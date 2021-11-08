6135d3ade25797dd91fe0149caed07005f2ac3a1
toli2003 <toli2003@student.miun.se>
2021-11-08 08:16:23 +0100

    Added comments.

--- PacMan/src/AbstractFactory.java
+++ PacMan/src/AbstractFactory.java
@@ -1,3 +1,10 @@
+
+/**
+ * The Abstract Factory, defining the two methods used for creating pacman and
+ * the ghosts.
+ * 
+ * @author Tobias Liljeblad & Rasmus Djupedal
+ */
 public abstract class AbstractFactory {
     abstract Pacman getCharacter(String type, int x, int y);
 
