c8fc1eaf7dd4e03362cf14ce10e0d88965d9ca17
toli2003 <toli2003@student.miun.se>
2021-11-08 08:19:24 +0100

    added comments.

--- PacMan/src/FactoryProducer.java
+++ PacMan/src/FactoryProducer.java
@@ -1,5 +1,16 @@
+
+/**
+ * The Factory producer, producing either a pacman or ghost Factory. Part of the
+ * Abstract Factory pattern.
+ */
 public class FactoryProducer {
 
+    /**
+     * Getter / Creator of factories. Creates either a pacman of ghost factory.
+     * 
+     * @param pacMan Boolean to determine what kind of factory to be created.
+     * @return either a pacmanfactory or ghostfactory.
+     */
     public static AbstractFactory getFactory(boolean pacMan) {
 
         if (pacMan) {
