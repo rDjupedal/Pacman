e90b87dc8a20d1e4441544ff8b1f93320aaede47
toli2003 <toli2003@student.miun.se>
2021-11-08 08:22:14 +0100

    addded comments

--- PacMan/src/GhostFactory.java
+++ PacMan/src/GhostFactory.java
@@ -1,11 +1,25 @@
+/**
+ * Concrete Ghost Factory, returns a new ghost.
+ * 
+ * @author Tobias Liljeblad & Rasmus Djupedal
+ */
+
 public class GhostFactory extends AbstractFactory {
 
+    /**
+     * used in PacManFActory. Returns null.
+     */
     @Override
     Pacman getCharacter(String type, int x, int y) {
 
         return null;
     }
 
+    /**
+     * Creation of a ghost.
+     * 
+     * @return a new instance of Ghost class.
+     */
     @Override
     Ghost getCharacter(String type, int x, int y, String color) {
         return new Ghost(x, y, color);
